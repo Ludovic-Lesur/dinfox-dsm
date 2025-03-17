007897e7b300d19a69b6765a2e3c8ec0e7798986
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-03-17 19:57:45 +0100

    Optimize terminal hardware driver.

--- drivers/utils/src/terminal_hw.c
+++ drivers/utils/src/terminal_hw.c
@@ -25,16 +25,11 @@ TERMINAL_status_t TERMINAL_HW_init(uint8_t instance, uint32_t baud_rate, TERMINA
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_init(baud_rate, rx_irq_callback);
-        LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
+    // Unused parameter.
+    UNUSED(instance);
+    // Init LMAC layer.
+    lmac_status = LMAC_init(baud_rate, rx_irq_callback);
+    LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
 errors:
     return status;
 }
@@ -44,17 +39,11 @@ TERMINAL_status_t TERMINAL_HW_de_init(uint8_t instance) {
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_de_init();
-        LMAC_stack_error(ERROR_BASE_TERMINAL + TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
-errors:
+    // Unused parameter.
+    UNUSED(instance);
+    // Release LMAC layer.
+    lmac_status = LMAC_de_init();
+    LMAC_stack_error(ERROR_BASE_TERMINAL + TERMINAL_ERROR_BASE_HW_INTERFACE);
     return status;
 }
 
@@ -63,16 +52,11 @@ TERMINAL_status_t TERMINAL_HW_enable_rx(uint8_t instance) {
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_enable_rx();
-        LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
+    // Unused parameter.
+    UNUSED(instance);
+    // Enable receiver.
+    lmac_status = LMAC_enable_rx();
+    LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
 errors:
     return status;
 }
@@ -82,16 +66,11 @@ TERMINAL_status_t TERMINAL_HW_disable_rx(uint8_t instance) {
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_disable_rx();
-        LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
+    // Unused parameter.
+    UNUSED(instance);
+    // Disable receiver.
+    lmac_status = LMAC_disable_rx();
+    LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
 errors:
     return status;
 }
@@ -101,16 +80,11 @@ TERMINAL_status_t TERMINAL_HW_write(uint8_t instance, uint8_t* data, uint32_t da
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_write(data, data_size_bytes);
-        LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
+    // Unused parameter.
+    UNUSED(instance);
+    // Write over LMAC layer.
+    lmac_status = LMAC_write(data, data_size_bytes);
+    LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
 errors:
     return status;
 }
@@ -121,16 +95,11 @@ TERMINAL_status_t TERMINAL_HW_set_destination_address(uint8_t instance, uint8_t 
     // Local variables.
     TERMINAL_status_t status = TERMINAL_SUCCESS;
     LMAC_status_t lmac_status = LMAC_SUCCESS;
-    // Check instance.
-    switch (instance) {
-    case TERMINAL_INSTANCE_LMAC:
-        lmac_status = LMAC_set_destination_address(destination_address);
-        LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
-        break;
-    default:
-        status = TERMINAL_ERROR_INSTANCE;
-        goto errors;
-    }
+    // Unused parameter.
+    UNUSED(instance);
+    // Set destination address.
+    lmac_status = LMAC_set_destination_address(destination_address);
+    LMAC_exit_error(TERMINAL_ERROR_BASE_HW_INTERFACE);
 errors:
     return status;
 }
