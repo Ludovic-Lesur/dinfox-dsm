4cecdfb783243546c8c37304e808a31bb42c7ed5
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-01-19 21:24:46 +0100

    Code formatting.

--- middleware/node/src/node.c
+++ middleware/node/src/node.c
@@ -170,6 +170,7 @@ static NODE_status_t _NODE_load_register(uint8_t reg_addr, uint32_t* reg_value) 
     nvm_status = NVM_read_word((NVM_ADDRESS_UNA_REGISTERS + reg_addr), reg_value);
     NVM_exit_error(NODE_ERROR_BASE_NVM);
 #else
+    // Reset output.
     (*reg_value) = 0;
     // Byte loop.
     for (idx = 0; idx < UNA_REGISTER_SIZE_BYTES; idx++) {
