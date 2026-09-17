7a90ed99f00d6a83f57a21adec9c1709794985c7
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2026-09-17 19:56:45 +0200

    Update CMSIS and DSP libraries.

---
+++ drivers/utils/inc/arm_compiler_specific.h
@@ -0,0 +1,13 @@
+/*
+ * arm_compiler_specific.h
+ *
+ *  Created on: 17 sep. 2026
+ *      Author: Ludo
+ */
+
+#ifndef __ARM_COMPILER_SPECIFIC_H__
+#define __ARM_COMPILER_SPECIFIC_H__
+
+#include "cmsis_compiler.h"
+
+#endif /* __ARM_COMPILER_SPECIFIC_H__ */
